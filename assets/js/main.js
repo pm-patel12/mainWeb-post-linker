0cb033c03be93886b2a088f1d5514430795ffcd6
unknown <pmp372418@gmail.com>
2023-10-14 17:16:23 +0530

    Home Page

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+$(document).on('ready', function () {
+    $(".sliderWrap").slick({
+        infinite: true,
+        loop:true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '200px',
+        nextArrow: `<button class="next"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-right" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <path d="M9 6l6 6l-6 6"></path>
+                    </svg></button>`,
+        prevArrow: `<button class="prev"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-left" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <path d="M15 6l-6 6l6 6"></path>
+                    </svg></button>`
+    })
+    // Testimonials
+    $("#testimonialRow1").slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    })
+    $("#testimonialRow2").slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        rtl: true,
+    })
+});
