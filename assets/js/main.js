cacf5aa82e2188e833cdfb5ce6e2b3aa29d3f4ca
pm-patel12 <pmp372418@gmail.com>
2023-10-25 01:25:31 +0530

    Pricing Page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,19 @@
 $(document).on('ready', function () {
     $(".sliderWrap").slick({
         infinite: true,
-        loop:true,
+        loop: true,
         slidesToShow: 3,
         slidesToScroll: 1,
         centerMode: true,
         centerPadding: '200px',
         nextArrow: `<button class="next"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-right" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                    <path d="M9 6l6 6l-6 6"></path>
-                    </svg></button>`,
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M9 6l6 6l-6 6"></path>
+                </svg></button>`,
         prevArrow: `<button class="prev"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-left" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                    <path d="M15 6l-6 6l6 6"></path>
-                    </svg></button>`
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M15 6l-6 6l6 6"></path>
+                </svg></button>`
     })
     // Testimonials
     $("#testimonialRow1").slick({
@@ -31,5 +31,17 @@ $(document).on('ready', function () {
     })
     // Header & Footer 
     $("#header").load("/ui/header.html");
-$("#footer").load("/ui/footer.html");
+    $("#footer").load("/ui/footer.html");
+    // Number Counter
+    $('.count').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 2000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
 });
